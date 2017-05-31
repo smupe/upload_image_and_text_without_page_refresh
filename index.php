d0e7bd888c3c08846887be2693b4ec18e72d278c
smupe <safari.mupe@gmail.com>
2017-05-31 12:39:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<form id="form_post">
+<textarea id="text_post" name="text_post" placeholder="share what you have"></textarea>
+<input type="file" id="files" name="files[]" multiple />
+<button id="btn_post" type="submit" >Post</button>
+</form>
